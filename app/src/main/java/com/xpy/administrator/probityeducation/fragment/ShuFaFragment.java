ff89b844e3899ec44ee40114960671e4c03bebf7
xpy <865934097@qq.com>
2017-11-21 22:15:49 +0800

    1、添加了gridview里面几张图片 2、修改了gridview里面的样式，简单修改 3、发现为解决的bug：    滑动切换会出现不显示情况    图片加载卡断，内存超出

--- app/src/main/java/com/xpy/administrator/probityeducation/fragment/ShuFaFragment.java
+++ app/src/main/java/com/xpy/administrator/probityeducation/fragment/ShuFaFragment.java
@@ -29,8 +29,8 @@ public class ShuFaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_shu_fa, container, false);
-        ButterKnife.bind(this,view);
+        View view = inflater.inflate(R.layout.fragment_shu_fa, container, false);
+        ButterKnife.bind(this, view);
         return view;
     }
 
@@ -38,11 +38,55 @@ public class ShuFaFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         list = new ArrayList<>();
-        MyGridViewAdapter adapter = new MyGridViewAdapter(getActivity(),list);
+        ImageTitleModel imageTitleModel1 = new ImageTitleModel(R.drawable.panyu, "17计科1潘宇");
+        ImageTitleModel imageTitleModel2 = new ImageTitleModel(R.drawable.liumengyuan, "17计科4刘梦圆");
+        ImageTitleModel imageTitleModel3 = new ImageTitleModel(R.drawable.chenyi, "17计科2陈怡");
+        ImageTitleModel imageTitleModel4 = new ImageTitleModel(R.drawable.xueyang, "14数师3薛杨 ");
+        ImageTitleModel imageTitleModel5 = new ImageTitleModel(R.drawable.yanjing, "14网工1严谨 ");
+        ImageTitleModel imageTitleModel6 = new ImageTitleModel(R.drawable.liuhui, "14软工6刘慧 ");
+        ImageTitleModel imageTitleModel7 = new ImageTitleModel(R.drawable.fanyuanyuan, "15软工6范圆圆 ");
+        ImageTitleModel imageTitleModel8 = new ImageTitleModel(R.drawable.jihaimin, "17计科1嵇海民 ");
+        //ImageTitleModel imageTitleModel9 = new ImageTitleModel(R.drawable.yujiaying, "17计科4余家颖");
+        //ImageTitleModel imageTitleModel10 = new ImageTitleModel(R.drawable.shicheng, "14软转石城");
+
+        //ImageTitleModel imageTitleModel11 = new ImageTitleModel(R.drawable.zhengjiaoyue, "14网工郑皎月");
+        //ImageTitleModel imageTitleModel12 = new ImageTitleModel(R.drawable.chenlei, "14卓工2陈蕾");
+        //ImageTitleModel imageTitleModel13 = new ImageTitleModel(R.drawable.wangxuechun, "17计科5王雪纯");
+        //ImageTitleModel imageTitleModel14 = new ImageTitleModel(R.drawable.wangying, "17计科5王颖");
+        ImageTitleModel imageTitleModel15 = new ImageTitleModel(R.drawable.suyifan, "17网工苏怡梵");
+        ImageTitleModel imageTitleModel16 = new ImageTitleModel(R.drawable.zhuchengliang, "17数师1朱成樑");
+        ImageTitleModel imageTitleModel17 = new ImageTitleModel(R.drawable.wutong, "17计科5吴彤");
+        ImageTitleModel imageTitleModel18 = new ImageTitleModel(R.drawable.zhangliang, "15软转1张靓");
+        ImageTitleModel imageTitleModel19 = new ImageTitleModel(R.drawable.yuyingchun, "17计科5于迎春");
+        ImageTitleModel imageTitleModel20 = new ImageTitleModel(R.drawable.wangruiyue, "17计科4王睿玥");
+
+        list.add(imageTitleModel1);
+        list.add(imageTitleModel2);
+        list.add(imageTitleModel3);
+        list.add(imageTitleModel4);
+        list.add(imageTitleModel5);
+        list.add(imageTitleModel6);
+        list.add(imageTitleModel7);
+        list.add(imageTitleModel8);
+//        list.add(imageTitleModel9);
+//        list.add(imageTitleModel10);
+//        list.add(imageTitleModel11);
+//        list.add(imageTitleModel12);
+//        list.add(imageTitleModel13);
+//        list.add(imageTitleModel14);
+        list.add(imageTitleModel15);
+        list.add(imageTitleModel16);
+        list.add(imageTitleModel17);
+        list.add(imageTitleModel18);
+        list.add(imageTitleModel19);
+        list.add(imageTitleModel20);
+
+        MyGridViewAdapter adapter = new MyGridViewAdapter(getActivity(), list);
         gvShuFa.setAdapter(adapter);
     }
 
-    public ShuFaFragment() {}
+    public ShuFaFragment() {
+    }
 
 
 }
--- app/src/main/java/com/xpy/administrator/probityeducation/utils/ImageUtils.java
+++ app/src/main/java/com/xpy/administrator/probityeducation/utils/ImageUtils.java
@@ -2,7 +2,7 @@ package com.xpy.administrator.probityeducation.utils;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.BitmapFactory;
 import android.media.ThumbnailUtils;
 
 import java.io.InputStream;
@@ -22,11 +22,16 @@ public class ImageUtils {
         //通过openRawResource获取一个InputStream对象
         InputStream input  = context.getResources().openRawResource(imageId);
         //通过InputStream创建BitmapDrawable对象
-        BitmapDrawable image = new BitmapDrawable(input);
+
+
+
+        //BitmapDrawable image = new BitmapDrawable(input);
         //通过BitmapDrawable对象获取Bitmap对象
-        Bitmap bitmap = image.getBitmap();
+        //Bitmap bitmap = image.getBitmap();
+
+        Bitmap bitmap = BitmapFactory.decodeStream(input);
         //利用Bitmap对象创建缩略图
-        bitmap = ThumbnailUtils.extractThumbnail(bitmap,60,60);
+        bitmap = ThumbnailUtils.extractThumbnail(bitmap,100,100);
         return bitmap;
     }
 }
